6eb49545059f4ec681b5bb8665bd0fbbc0c6988a
James <jismith1989@googlemail.com>
2019-07-02 15:28:58 +0100

    First commit; working project

---
+++ age.go
@@ -0,0 +1,42 @@
+package age
+
+import "time"
+
+const (
+	leapYearDay = 60
+)
+
+// Calculate provides an age from the present time to a given time.
+func Calculate(givenTime time.Time) int64 {
+	presentTime := time.Now().UTC()
+	age := int64(presentTime.Year()) - int64(givenTime.Year())
+
+	givenYearDay := givenTime.YearDay()
+	presentYearDay := presentTime.YearDay()
+
+	if isLeapYear(givenTime) && !isLeapYear(presentTime) && givenYearDay >= leapYearDay {
+		givenYearDay--
+	} else if isLeapYear(presentTime) && !isLeapYear(givenTime) && presentYearDay >= leapYearDay {
+		givenYearDay++
+	}
+
+	if presentYearDay < givenYearDay {
+		age--
+	}
+
+	return age
+}
+
+func isLeapYear(givenTime time.Time) bool {
+	year := givenTime.Year()
+
+	if year%400 == 0 {
+		return true
+	} else if year%100 == 0 {
+		return false
+	} else if year%4 == 0 {
+		return true
+	}
+
+	return false
+}
